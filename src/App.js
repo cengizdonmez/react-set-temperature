f07537c3061bd07478b642a08483848e13b1c7b4
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-09-09 10:35:48 +0300

    Temperature setting created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+
 function App() {
-  return <div className="App">Initial Commit</div>;
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
+    const newTemperature = temperatureValue + 1;
+    if (newTemperature >= 15) {
+      setTemperatureColor('hot');
+    }
+    setTemperatureValue(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+    const newTemperature = temperatureValue - 1;
+    if (newTemperature < 15) {
+      setTemperatureColor('cold');
+    }
+    setTemperatureValue(newTemperature);
+  };
+
+  return (
+    <div className="app">
+      <div className={`temperature ${temperatureColor}`}>
+        <div className="temperature-display">{temperatureValue}°C</div>
+
+        <div className="set-buttons">
+          <button onClick={() => increaseTemperature()}>+</button>
+          <button onClick={() => decreaseTemperature()}>-</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
